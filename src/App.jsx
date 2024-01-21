d8b25a13ad95d1999f8bbe4b95aa7fe4c74c04a3
drewhouses <drewcasas@live.com>
2024-01-21 10:53:28 -0600

    first commit, added skeleton and some boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+// import <component> from './components/<comp_name>';
+
+function App() {
+  return (
+    <div className="portfolio">{/* {Portfolio Component goes here} */}</div>
+  );
+}
+
+export default App;
