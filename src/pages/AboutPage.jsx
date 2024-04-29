348f4d5dde31747f4625de489083c50cc4cc215c
drewhouses <drewcasas@live.com>
2024-04-29 18:21:37 -0500

    Added more info to about-me page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -11,55 +11,32 @@ export default function AboutPage() {
         developer. My expertise in software support has definitely given me an
         edge as I have a great amount of knowledge and expertise dealing with
         different enterprise platforms. This has helped me gain an important
-        foundational knowledge in computing before taking on the challenge of
-        becoming a web developer.
+        foundational knowledge in how things work before taking on the challenge
+        of becoming a web developer.
       </p>
       <h3>Education</h3>
       <p className="aboutme-text">
         I graduated from Rochester Institute of Technology with a degree in
-        Networking and Systems Administration. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Networking and Systems Administration. During my time here I also gained
+        experience building in languages like C++, Java, HTML/CSS, Python, and
+        Perl.
       </p>
       <p className="aboutme-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        I recently completed a Full-Stack Development Coding Bootcamp to help
+        jumpstart my career in web development. Throughout this course, I worked
+        with the foundational technologies like React, MongoDB, GraphQL, MySQL,
+        ExpressJS, and many others. This allowed me to build various
+        applications, some of which you can check out on my portfolio page.
+      </p>
+      <h3>Getting in touch</h3>
+      <p className="aboutme-text">
+        If you would like to see my official resume, you can download from the
+        "Resume" page OR If you just want to reach out and get some more
+        information from me, you can send me a message from the "Contact" page.
       </p>
       <p className="aboutme-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p className="aboutme-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p className="aboutme-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Lastly, you can also check out my X, GitHub, or LinkedIn accounts with
+        the links found in the footer.
       </p>
     </div>
   );
